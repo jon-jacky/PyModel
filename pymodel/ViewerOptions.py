691e49247a02ccc598c7bb41a43f5352ecc3252a
Jon Jacky <jon@u.washington.edu>
2012-10-15 08:42:36 -0700

    add new command pymodel viewer, pmv

---
+++ pymodel/ViewerOptions.py
@@ -0,0 +1,47 @@
+"""
+Options and arguments for PyModel Viewer
+"""
+
+from optparse import OptionParser
+
+usage = """pmv.py [options] models  
+
+PyModel Viewer.  A single program that invokes pma, pmg, and graphviz
+dot to to perform analysis, generate dot commands, and generate a file
+of graphics in .svg, .pdf or another format.  This program accepts all
+of the command line options and arguments of both pma and pmg, and the
+-T option of dot (to select the graphics format), then passes the
+options to the appropriate program."""
+
+parser = OptionParser(usage=usage)
+
+def parse_args():
+
+  # options for pma from AnalyzerOptions
+  parser.add_option('-a', '--action', action="append", 
+                  help='Action to include in generated FSM, as many as needed, if no -a include all actions')
+  parser.add_option('-e', '--exclude', action="append", 
+                  help='Action to exclude from generated FSM, as many as needed')
+  parser.add_option('-m', '--maxTransitions', type="int", default=100, 
+                  help = 'Maximum number of transitions to include in the generated FSM, default 100')
+  parser.add_option('-o', '--output', type='string', default='',
+                  help = 'Output file basename (before the .foo suffix), default is <first argument>FSM')
+
+  # options for pmg from GraphicsOptions
+  # -o --output is already handled above
+  parser.add_option('-l', '--transitionLabels', type='string', 
+                      default='action',
+                      help = 'Transition labels: action, name, or none, default is action')
+  parser.add_option('-x', '--noStateTooltip', action="store_true", dest="noStateTooltip", 
+                    help = 'Omit tooltips from state bubbles (to work around dot svg problem)')
+  parser.add_option('-y', '--noTransitionTooltip', action="store_true", dest="noTransitionTooltip", 
+                    help = 'Omit tooltips from transition arrows')
+
+  # options from graphviz dot
+  parser.add_option ('-T', '--fileType', type='string', default='svg',
+                     help = 'Graphics file type (format), default svg')
+
+  return parser.parse_args()
+
+def print_help():
+  parser.print_help()  # must have at least one arg, not optional
