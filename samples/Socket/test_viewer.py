4769ccff26ad4e6468357b38e50a758bbec3526a
Jon Jacky <jon@u.washington.edu>
2012-11-17 14:49:55 -0800

    test_viewer.py: add a test case, fix a bug version previously in git would not run due to missing comma between cases also, add initial test case without -e options, to show their effect

--- samples/Socket/test_viewer.py
+++ samples/Socket/test_viewer.py
@@ -3,8 +3,11 @@ Socket model viewer  tests
 """
 
 cases = [
+    ('No -e options',
+     'pmv.py -o send_aa_small_no_e_FSM send_aa_small Socket len_buffers_two'),
+
     ('Two -e options in a single command',
-     'pmv.py send_aa_small Socket len_buffers_two -e send_close -e recv_close')
+     'pmv.py send_aa_small Socket len_buffers_two -e send_close -e recv_close'),
 
     ('Same graph, this time generate PDF',
      'pmv.py -T pdf send_aa_small Socket len_buffers_two -e send_close -e recv_close')
