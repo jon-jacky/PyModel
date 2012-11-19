3952a9f2aa6894ac56b15f691e7cb4c8a3380d74
Jon Jacky <jon@u.washington.edu>
2012-11-19 09:15:50 -0800

    WebApplication tests: regenerate .ref files old .ref files were generated on a windows system new .ref files generated on Mac OS X random seed command line option apparently has different effect also changed test_stepper.py comments to be current, consistent .ref files now include stderr as well as stdout

--- samples/WebApplication/test_stepper.py
+++ samples/WebApplication/test_stepper.py
@@ -8,13 +8,18 @@ run webapp.py in this sample with the wsgirunner server:
   wsgirunner.py -p 8080 webapp
 
 then you can run this test script.
+
+Some runs may or may not end in accepting state (so they may succeed or fail),
+this should be reproducible on a given system (because the random seed
+is given in the command line -s option) but is not reproducible on different
+systems.
 """
 
 cases = [
- ('Test: WebModel on-the-fly ends in accepting state, should succeed',
+ ('Test: WebModel on-the-fly run, may or may not end in accepting state',
    'pmt.py -n 3 -s 5 -i Stepper WebModel'),
 
- ('Test: WebModel on-the-fly ends in non-accepting state, should fail',
+ ('Test: WebModel on-the-fly run, may or may not end in accepting state',
    'pmt.py -n 3 -s 2 -i Stepper WebModel'),
 
  ('Test: TestIntSuccess offline test suite, with Stepper, should succeed',
@@ -26,7 +31,7 @@ cases = [
  ('Test: TestLoginFiveFailures offline test suite, with Stepper, should succeed',
   'pmt.py TestLoginFiveFailures -i Stepper'),
 
- ('Test: WebModel on-the-fly with all actions, with Stepper, should fail',
+ ('Test: WebModel on-the-fly run, may or may not end in accepting state',
   'pmt.py -n 20 -s 1 -i Stepper WebModel'),
 
  ('WebModel TestIntTwoRuns offline test suite with two runs',
