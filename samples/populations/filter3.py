1ff9dd3c9206f8fac2c9fe303f65f2e8d7f74184
Jon Jacky <jon@u.washington.edu>
2012-11-19 21:52:02 -0800

    abp, populations samples: tweak tests, regenerate .ref add populations/filter_lc.py to test_filter to demonstrate bug where lowercase 'statefilter' identifier does not work in config files

--- samples/populations/filter3.py
+++ samples/populations/filter3.py
@@ -5,6 +5,6 @@ PyModel configuration - state filter, limit population to 3
 import populations
 
 def filter3():
-    return len(populations) <= 3
+    return len(populations.population) <= 3
 
-populations.statefilter = filter3
+populations.StateFilter = filter3
