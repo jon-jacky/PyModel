03bf5b6d8885a6b19eac950ba784d62bec4d2bcf
Jon Jacky <jon@u.washington.edu>
2012-12-04 21:28:50 -0800

    add more test cases for -e and -a with FSM

--- samples/Socket/test_e_option.py
+++ samples/Socket/test_e_option.py
@@ -6,7 +6,13 @@ cases = [
     ('No -e options: pmt -n 10 NoBlockScenario',
      'pmt -n 10 NoBlockScenario'),
 
-    ('Now same command with -e send_close -e recv_close',
+    ('Same FSM with -e send_close -e recv_close',
      'pmt -e send_close -e recv_close -n 10 NoBlockScenario'),
+
+    ('Same FSM with -e send_close -e recv_close, also -e recv_call',
+     'pmt -e send_close -e recv_close -e recv_call -n 10 NoBlockScenario'),
+
+    ('Same FSM with -a send_close -a recv_close',
+     'pmt -a send_close -a recv_close -n 10 NoBlockScenario'),
 ]
 
--- samples/Socket/test_e_viewer.py
+++ samples/Socket/test_e_viewer.py
@@ -1,12 +1,17 @@
 """
-Test -e option in FSMs, using pmv
+Test -e and -a options in FSM, using pmv
 """
 
 cases = [
-    ('No -e option',
-     'pmv NoBlockScenario'),
+    ('No -e options: pmv  NoBlockScenario',
+     'pmv  NoBlockScenario'),
 
-    ('Now same command with -e send_close -e recv_close',
-    'pmv -e send_close -e recv_close NoBlockScenario -o NoBlockScenario_e'),
+    ('Same FSM with -e send_close -e recv_close',
+     'pmv -e send_close -e recv_close  NoBlockScenario -o NoBlockScenario_e'),
+
+    ('Same FSM with -e send_close -e recv_close, also -e recv_call',
+     'pmv -e send_close -e recv_close -e recv_call NoBlockScenario -o NoBlockScenario_e_e'),
+
+    ('Same FSM with -a send_close -a recv_close',
+     'pmv -a send_close -a recv_close  NoBlockScenario -o NoBlockScenario_a'),
 ]
-
