a57e920ee14e80d365b3845b54c812ccafb4c21d
Jon Jacky <jon@u.washington.edu>
2013-02-15 20:20:23 -0800

    continue cleanup, git rm more files

--- samples/Socket/len_buffers_two.py
+++
@@ -1,8 +0,0 @@
-"""pymodel config"""
-
-import Socket
-
-def len_buffers_two():
-    return len(Socket.buffers) <= 2
-
-Socket.StateFilter = len_buffers_two
--- samples/Socket/len_buffers_two_pep8.py
+++
@@ -1,8 +0,0 @@
-"""pymodel config"""
-
-import Socket
-
-def len_buffers_two():
-    return len(Socket.buffers) <= 2
-
-Socket.state_filter = len_buffers_two
