502e65b4da49cffa0ae1806d3947df5cd4ce54c4
Jon Jacky <jon@u.washington.edu>
2013-03-09 13:09:31 -0800

    same socket_simulator works with stepper_s and stepper_a merge socket_simulator_s and socket_simulator, then rm socket_simulator_s also separate out select body from socket_simulator_s to select_simulator

--- samples/Socket/socket_simulator.py
+++ samples/Socket/socket_simulator.py
@@ -1,21 +1,22 @@
 """
 socket_simulator.py
 
+Socket simulator that works with stepper_s asynchronous stepper that
+uses select, or stepper_a asynchronous stepper that uses threads.
+
 Replacement for the Python standard library socket module to use with
 the PyModel socket sample, to demonstrate nondeterminism and blocking
 even with small messages.
 
 The simulated socket here nondeterministically sends or receives just
 part of the message, no matter how short the message (even just
-two characters).   The socket blocks on write when buffers are full, and
+two characters).   
+
+(?) The socket blocks on write when buffers are full, and
 the buffers can be made very small, by assigning the bufsize variable.
 The default bufsize is 3 characters.
 
-When used with the asynchronous stepper (stepper_a), send (and recv)
-are called from different threads in the stepper, so that even if the
-call blocks because the buffer is full (or empty), it doesn't block
-pmt (the test run).  This enables a blocked send (or recv) call to be
-unblocked by a later call to recv (or send).
+...
 
 To use this simulator, just put it in the same directory with your
 PyModel socket steppers and rename it (or symlink it) to socket.py.
@@ -24,7 +25,6 @@ module.
 """
 
 import random    # for nondeterministic behavior
-import threading # for blocking on send/full or recv/empty 
 
 """
 Configure nondeterminism and blocking 
@@ -35,10 +35,12 @@ and all the storage in the network between.
 All sockets share the *same* buffer - intended to have just one 
 client socket and one server socket at a time -
 """
-buffers = ''  # initially the buffers are empty
-nondet = True # False to send/recv entire msg every time
-bufsize = 3   # send blocks when buffer full, recv blocks when empty
+
+import threading # event e for blocking on send/full or recv/empty 
+
 e = threading.Event() # initially false, e.wait() to block
+
+nondet = True # False to send/recv entire msg every time
 
 # used by steppers, here they are all dummies
 AF_INET = 0
@@ -48,42 +50,54 @@ SO_REUSEADDR = 0
 SO_RCVBUF = 0
 SO_SNDBUF = 0
 
-class connection(object):
-    def __init__(self, *args): pass
+# buffers has to be one mutable object so it can be shared by send and recv 
+buffers = list('')  # list is mutable
+bufsize = 3   # send blocks when buffer full, recv blocks when empty
 
+class socket(object):
+
+    def __init__(self, *args): 
+        self.buffers = buffers  # all instances have reference to same global buffers
+    
     def send(self, msg):
-        global buffers
-        free = bufsize - len(buffers)
-        # send blocks if buffer full
-        # Asynch stepper - don't just block here, instead wait for free > 0
+        """
+        send must block if buffer full
+        so select must ensure that send is not called if buffer is full
+        """
+        free = bufsize - len(self.buffers)
+        # print 'send: msg "%s", buffers "%s", bufsize - len(self.buffers) %d' % \
+        #    (msg, ''.join(self.buffers), free) #DEBUG
+        # assert free > 0 # in stepper_s, select ensures send is not called if buffer is full
         while free <= 0:
             e.wait(1) # 1 second timeout, block without spinning
             free = bufsize - len(buffers)
         # nondeterministically choose prefix of msg to send
-        msglen = min(len(msg),free)
-        msglen = random.randint(1,msglen) if nondet else msglen
-        buffers += msg[:msglen]
-        return msglen
+        nmax = min(len(msg),free)
+        nsent = random.randint(1,nmax) if nondet else nmax
+        # print 'send: nsent %d' % nsent
+        self.buffers.extend(list(msg[:nsent])) # mutate list of characters
+        return nsent
 
     def recv(self, nmax):
+        """
+        recv must block if buffer empty
+        so select must ensure that 
+        send is not called if buffer is empty
+        """
         global buffers
-        # recv blocks if buffer empty
+        #print 'recv: buffers "%s", len(self.buffers) %d' % \
+        #   (''.join(self.buffers), len(self.buffers)) #DEBUG 
+        # assert len(self.buffers) > 0 # in stepper_s, select ensures recv is not called if buffer is empty 
         while len(buffers) <= 0:
             e.wait(1) # 1 second timeout, block without spinning
         # nondeterministically choose suffix of buffers to recv
-        msglen = min(nmax,len(buffers))
+        msglen = min(nmax,len(self.buffers))
         msglen = random.randint(1,msglen) if nondet else msglen
-        msg = buffers[:msglen]
-        buffers = buffers[msglen:]
+        msg = ''.join(self.buffers[:msglen]) # make string from part of list
+        del self.buffers[:msglen] # mutate list of characters
         return msg
 
     def close(self): pass
-
-
-class socket(connection):
-
-    def __init__(self, *args): 
-        connection.__init__(self, *args)
 
     # client and server can both use these
     def getsockopt(self, *args): return None
@@ -99,6 +113,4 @@ class socket(connection):
     def listen(self, *args): pass
 
     def accept(self, *args):
-        c = connection(*args)
-        return c, 'nowhere.simulator.org'
-
+        return self, 'nowhere.simulator.org'
