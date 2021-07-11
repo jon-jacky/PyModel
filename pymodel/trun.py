7c949757dfd517e8ca0d82d2182d1314b6c6c864
Zohar Lorberbaum <zlorberbaum@qumu.com>
2021-07-10 23:30:04 -0700

    trun input validation

--- pymodel/trun.py
+++ pymodel/trun.py
@@ -6,7 +6,15 @@ import os
 # argv[1] is name of module containing test cases
 # dir containing this module must be on PYTHONPATH
 
-test = __import__(sys.argv[1])
+if len(sys.argv) != 2:
+  print('\nUsage: trun <test_cases_module>\n\n')
+  sys.exit()
+
+try:
+  test = __import__(sys.argv[1])
+except ModuleNotFoundError:
+  print('\nCould not find tests file "{}".\n\n'.format(sys.argv[1]))
+  sys.exit()
 
 # Test cases are in 'cases', a list of pairs of strings, descrip. and commmand:
 # cases = [
@@ -14,7 +22,11 @@ test = __import__(sys.argv[1])
 #   'pct.py -n 10 PowerSwitch'),
 #  ... ]
 
-for (description, cmd) in test.cases: 
-  print(description)
-  os.system(cmd)
-  print()
+try:
+  for (description, cmd) in test.cases: 
+    print(description)
+    os.system(cmd)
+    print()
+except AttributeError:
+  print('\nCould not find test cases in "{}".\n\n'.format(sys.argv[1]))
+  sys.exit()
